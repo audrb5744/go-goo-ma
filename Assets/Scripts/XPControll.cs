050dcf85c1b64b3f8ac279776177cb7b43fb5ee3
DNO <70924789+audrb5744@users.noreply.github.com>
2023-06-27 15:36:37 +0900

    Update XPControll.cs

--- Assets/Scripts/XPControll.cs
+++ Assets/Scripts/XPControll.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class XPControll : MonoBehaviour
 {
   public float xp = 0f;
   public float needxp = 3f;
   public int level = 1;
+  public Slider xpSlider;
 
-// void Update()
-//     {
-      
-//     }
+
+void Update()
+     {
+      xpSlider.value = xp;
+     }
 
 }
