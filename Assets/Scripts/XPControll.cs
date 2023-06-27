31eb170b21dd36435023ee9044e664d097d6d4af
DNO <70924789+audrb5744@users.noreply.github.com>
2023-06-27 16:07:34 +0900

    Update XPControll.cs

--- Assets/Scripts/XPControll.cs
+++ Assets/Scripts/XPControll.cs
@@ -10,10 +10,31 @@ public class XPControll : MonoBehaviour
   public int level = 1;
   private Slider xpSlider;
 
+void Start()
+{
+    xpSlider.onValueChanged.AddListener(OnSliderValueChanged);
+}
+
+void OnSliderValueChanged(float value)
+{
+    if (value == 0)
+    {
+        Color fillAreaColor = xpSlider.fillRect.GetComponent<Image>().color;
+        fillAreaColor.a = 0; // Set alpha to 0 to make the fill area invisible
+        xpSlider.fillRect.GetComponent<Image>().color = fillAreaColor;
+    }
+    else
+    {
+        Color fillAreaColor = xpSlider.fillRect.GetComponent<Image>().color;
+        fillAreaColor.a = 1; // Set alpha to 1 to make the fill area visible
+        xpSlider.fillRect.GetComponent<Image>().color = fillAreaColor;
+    }
+}
+
 
 void Update()
      {
-      xpSlider.value = xp;
+      xpSlider.value = 3;
      }
 
 }
